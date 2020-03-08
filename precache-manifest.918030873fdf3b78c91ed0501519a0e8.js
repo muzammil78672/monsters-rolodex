d46285e239d5083f1ad26a4f96a73d0392a88b7c
Muzammil Hussain Chandorewala <cmuzammil786@gmail.com>
2020-03-08 15:31:41 +0530

    Updates

---
+++ precache-manifest.918030873fdf3b78c91ed0501519a0e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2da2aca7abba3944512df7374534919",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2b210de6e44b4dc4e979",
+    "url": "/monsters-rolodex/static/css/main.0ca875f2.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b210de6e44b4dc4e979",
+    "url": "/monsters-rolodex/static/js/main.72c4e0d1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
